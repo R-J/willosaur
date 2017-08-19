7d35f7873307d1fa3594667631243810b3afc217
Arthur Freitas <email@arthr.me>
2017-08-19 18:51:13 -0300

    Adds support for Vanilla 2.3.x

---
+++ about.php
@@ -0,0 +1,10 @@
+<?php if (!defined('APPLICATION')) exit();
+
+$ThemeInfo['default'] = array(
+    'Name' => 'Willosaur',
+    'Description' => "The theme used in the Esporo forums.",
+    'Version' => 1.0,
+    'Author' => "Arthur Freitas",
+    'AuthorEmail' => 'email@arthr.me',
+    'AuthorUrl' => 'https://arthr.me/'
+);
