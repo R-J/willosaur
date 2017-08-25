0e93bbfa167d89a42861a1917f5282e734154324
R-J <robin.jurinka@gmx.net>
2017-08-25 12:50:02 +0200

    Attach footer module to non-admin pages

--- class.willosaurthemehooks.php
+++ class.willosaurthemehooks.php
@@ -4,4 +4,16 @@ class WillosaurThemeHooks implements Gdn_IPlugin {
     public function setup() {
     }
 
+    public function base_render_before($sender) {
+        if ($sender->MasterView == 'admin') {
+            return;
+        }
+        if (c('willosauer.ShowFooterModue', true) == false) {
+            return;
+        }
+
+        require_once __DIR__.'/modules/class.esporofootermodule.php';
+        $footerModule = new EsporoFooterModule();
+        $sender->addModule($footerModule);
+    }
 }
