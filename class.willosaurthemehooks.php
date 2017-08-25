fa9b9630147b88fe696b51d51f8f26c1ef58ddbc
R-J <robin.jurinka@gmx.net>
2017-08-25 15:18:54 +0200

    Override sprite function to be able to use SVGS instead of sprites

--- class.willosaurthemehooks.php
+++ class.willosaurthemehooks.php
@@ -17,3 +17,13 @@ class WillosaurThemeHooks implements Gdn_IPlugin {
         $sender->addModule($footerModule);
     }
 }
+
+function sprite($name, $type = 'Sprite', $text = false) {
+    include_once __DIR__.'design/svg/'.$name.'.svg';
+    $sprite = '<svg class="'.$type.' '.$name.'"><use xlink:href="#sprite-'.$name.'" /></svg>';
+    if ($text) {
+        $sprite .= '<span class="sr-only">'.$text.'</span>';
+    }
+
+    return $sprite;
+}
