34569835132ad5288d0d6b62de49dab1a46243a1
R-J <robin.jurinka@gmx.net>
2017-08-25 11:07:53 +0200

    Make version number a string

--- about.php
+++ about.php
@@ -3,7 +3,7 @@
 $ThemeInfo['willosaur'] = array(
     'Name' => 'Willosaur',
     'Description' => "The theme used in the Esporo forums.",
-    'Version' => 1.0,
+    'Version' => '1.0',
     'Author' => "Arthur Freitas",
     'AuthorEmail' => 'email@arthr.me',
     'AuthorUrl' => 'https://arthr.me/'
